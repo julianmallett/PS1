60fe5411265846bded8f7eb1e3972147e2c5f477
Julian Mallett <julian.mallett@akiranet.uk>
2018-05-13 23:03:33 +0100

    adding functions

---
+++ Functions1.ps1
@@ -0,0 +1,15 @@
+﻿function Do-Something {
+
+  param($Thing)
+
+  Write-Output "I did something to $Thing"
+
+  }
+function Set-Something {
+
+  param($Thing)
+
+  Write-Output "I set something on $Thing"
+
+  }
+
